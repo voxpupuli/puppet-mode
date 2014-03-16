0313b9b23da5aff6d299c19eb5ae391d3aa75e2d
Sebastian Wiesner <lunaryorn@gmail.com>
2014-03-16 23:36:00 +0100

    Add font lock tests

--- test/puppet-mode-test.el
+++ test/puppet-mode-test.el
@@ -35,15 +35,246 @@
 
 ;;;; Utilties
 
-(defmacro puppet-test-with-temp-buffer (contents &rest body)
+(defmacro puppet-test-with-temp-buffer (content &rest body)
   "Evaluate BODY in a temporary buffer with CONTENTS."
   (declare (debug t)
            (indent 1))
   `(with-temp-buffer
-     (insert ,contents)
+     (insert ,content)
      (goto-char (point-min))
      (puppet-mode)
      ,@body))
+
+(defun puppet-test-face-at (pos &optional content)
+  "Get the face at POS in CONTENT.
+
+If CONTENT is not given, return the face at POS in the current
+buffer."
+  (if content
+      (puppet-test-with-temp-buffer content
+        (font-lock-fontify-buffer)
+        (get-text-property pos 'face))
+    (font-lock-fontify-buffer)
+    (get-text-property pos 'face)))
+
+
+;;;; Font locking
+
+(ert-deftest puppet-mode-syntax-table/fontify-dq-string ()
+  (should (eq (puppet-test-face-at 8 "$foo = \"bar\"") 'font-lock-string-face)))
+
+(ert-deftest puppet-mode-syntax-table/fontify-sq-string ()
+  (should (eq (puppet-test-face-at 8 "$foo = 'bar'") 'font-lock-string-face)))
+
+(ert-deftest puppet-mode-syntax-table/fontify-line-comment ()
+  (puppet-test-with-temp-buffer "# class
+bar"
+    (should (eq (puppet-test-face-at 1) 'font-lock-comment-delimiter-face))
+    (should (eq (puppet-test-face-at 3) 'font-lock-comment-face))
+    (should (eq (puppet-test-face-at 7) 'font-lock-comment-face))
+    (should (eq (puppet-test-face-at 8) 'font-lock-comment-face))
+    (should-not (puppet-test-face-at 9))))
+
+(ert-deftest puppet-mode-syntax-table/fontify-c-style-comment ()
+  (puppet-test-with-temp-buffer "/*
+class */ bar"
+    (should (eq (puppet-test-face-at 1) 'font-lock-comment-face))
+    (should (eq (puppet-test-face-at 4) 'font-lock-comment-face))
+    (should (eq (puppet-test-face-at 8) 'font-lock-comment-face))
+    (should (eq (puppet-test-face-at 11) 'font-lock-comment-face))
+    (should-not (puppet-test-face-at 13))))
+
+(ert-deftest puppet-font-lock-keywords/regular-expression-literal ()
+  (puppet-test-with-temp-buffer "$foo =~ / class $foo/ {"
+    ;; The opening slash
+    (should (eq (puppet-test-face-at 9) 'puppet-regular-expression-literal))
+    ;; A keyword inside a regexp literal
+    (should (eq (puppet-test-face-at 11) 'puppet-regular-expression-literal))
+    ;; A variable inside a regexp literal
+    (should (eq (puppet-test-face-at 17) 'puppet-regular-expression-literal))
+    ;; The closing delimiter
+    (should (eq (puppet-test-face-at 21) 'puppet-regular-expression-literal))
+    ;; The subsequent brace
+    (should-not (puppet-test-face-at 23))))
+
+(ert-deftest puppet-font-lock-keywords/keyword-in-symbol ()
+  (should-not (puppet-test-face-at 4 "fooclass")))
+
+(ert-deftest puppet-font-lock-keywords/keyword-in-parameter-name ()
+  (should-not (puppet-test-face-at 1 "node_foo => bar")))
+
+(ert-deftest puppet-font-lock-keywords/keyword-as-parameter-name ()
+  ;; We don't highlight parameters in any specific way, so the keyword will get
+  ;; highlighted.  The reason is that keywords are keywords in selectors and
+  ;; hashes, but variables in resources, and since both statements use the same
+  ;; arrow-style syntax for key-value pairs, it's impossible to get highlighting
+  ;; right without looking at the surrounding syntactic context, which is way
+  ;; too much of an effort for too little gain.  See
+  ;; https://github.com/lunaryorn/puppet-mode/issues/36 for details
+  (should (eq (puppet-test-face-at 1 "unless => bar") 'font-lock-keyword-face)))
+
+(ert-deftest puppet-font-lock-keywords/simple-variable ()
+  (puppet-test-with-temp-buffer "$foo = bar"
+    (should (eq (puppet-test-face-at 1) 'font-lock-variable-name-face))
+    (should (eq (puppet-test-face-at 4) 'font-lock-variable-name-face))
+    ;; The operator should not get highlighted
+    (should-not (puppet-test-face-at 6))))
+
+(ert-deftest puppet-font-lock-keywords/variable-with-scope ()
+  (puppet-test-with-temp-buffer "$foo::bar"
+    (should (eq (puppet-test-face-at 1) 'font-lock-variable-name-face))
+    (should (eq (puppet-test-face-at 2) 'font-lock-variable-name-face))
+    ;; Scope operator
+    (should (eq (puppet-test-face-at 5) 'font-lock-variable-name-face))
+    (should (eq (puppet-test-face-at 7) 'font-lock-variable-name-face))))
+
+(ert-deftest puppet-font-lock-keywords/variable-in-top-scope ()
+  (puppet-test-with-temp-buffer "$::foo"
+    (should (eq (puppet-test-face-at 1) 'font-lock-variable-name-face))
+    (should (eq (puppet-test-face-at 2) 'font-lock-variable-name-face))
+    (should (eq (puppet-test-face-at 4) 'font-lock-variable-name-face))))
+
+(ert-deftest puppet-font-lock-keywords/class ()
+  (puppet-test-with-temp-buffer "class foo::bar
+{"
+    ;; The keyword
+    (should (eq (puppet-test-face-at 1) 'font-lock-keyword-face))
+    ;; The scope
+    (should (eq (puppet-test-face-at 7) 'font-lock-type-face))
+    ;; The scope operator
+    (should (eq (puppet-test-face-at 10) 'font-lock-type-face))
+    ;; The name
+    (should (eq (puppet-test-face-at 12) 'font-lock-type-face))
+    ;; The braces
+    (should-not (puppet-test-face-at 16))))
+
+(ert-deftest puppet-font-lock-keywords/define ()
+  (puppet-test-with-temp-buffer "define foo::bar($foo) {"
+    ;; The keyword
+    (should (eq (puppet-test-face-at 1) 'font-lock-keyword-face))
+    ;; The scope
+    (should (eq (puppet-test-face-at 8) 'font-lock-type-face))
+    ;; The scope operator
+    (should (eq (puppet-test-face-at 11) 'font-lock-type-face))
+    ;; The name
+    (should (eq (puppet-test-face-at 13) 'font-lock-type-face))
+    ;; The parenthesis
+    (should-not (puppet-test-face-at 16))
+    ;; The parameter
+    (should (eq (puppet-test-face-at 17) 'font-lock-variable-name-face))
+    (should-not (puppet-test-face-at 21))))
+
+(ert-deftest puppet-font-lock-keywords/node ()
+  (puppet-test-with-temp-buffer "node puppet.example.com {"
+    (should (eq (puppet-test-face-at 1) 'font-lock-keyword-face))
+    (should (eq (puppet-test-face-at 6) 'font-lock-type-face))
+    (should (eq (puppet-test-face-at 12) 'font-lock-type-face))
+    (should (eq (puppet-test-face-at 23) 'font-lock-type-face))
+    (should-not (puppet-test-face-at 25))))
+
+(ert-deftest puppet-font-lock-keywords/resource ()
+  (puppet-test-with-temp-buffer "foo::bar {"
+    (should (eq (puppet-test-face-at 1) 'font-lock-type-face))
+    (should (eq (puppet-test-face-at 4) 'font-lock-type-face))
+    (should (eq (puppet-test-face-at 6) 'font-lock-type-face))
+    (should-not (puppet-test-face-at 10))))
+
+(ert-deftest puppet-font-lock-keywords/resource-default ()
+  (puppet-test-with-temp-buffer "Foo::Bar {"
+    (should (eq (puppet-test-face-at 1) 'font-lock-type-face))
+    (should (eq (puppet-test-face-at 4) 'font-lock-type-face))
+    (should (eq (puppet-test-face-at 6) 'font-lock-type-face))
+    (should-not (puppet-test-face-at 10))))
+
+(ert-deftest puppet-font-lock-keywords/resource-default-not-capitalized ()
+  (puppet-test-with-temp-buffer "Foo::bar {"
+    (should-not (puppet-test-face-at 1))
+    (should-not (puppet-test-face-at 4))))
+
+(ert-deftest puppet-font-lock-keywords/resource-collector ()
+  (puppet-test-with-temp-buffer "Foo::Bar <|"
+    (should (eq (puppet-test-face-at 1) 'font-lock-type-face))
+    (should (eq (puppet-test-face-at 4) 'font-lock-type-face))
+    (should (eq (puppet-test-face-at 6) 'font-lock-type-face))
+    (should-not (puppet-test-face-at 10))))
+
+(ert-deftest puppet-font-lock-keywords/exported-resource-collector ()
+  (puppet-test-with-temp-buffer "Foo::Bar <<|"
+    (should (eq (puppet-test-face-at 1) 'font-lock-type-face))
+    (should (eq (puppet-test-face-at 4) 'font-lock-type-face))
+    (should (eq (puppet-test-face-at 6) 'font-lock-type-face))
+    (should-not (puppet-test-face-at 10))
+    (should-not (puppet-test-face-at 11))))
+
+(ert-deftest puppet-font-lock-keywords/resource-collector-not-capitalized ()
+  (puppet-test-with-temp-buffer "Foo::bar <<|"
+    (should-not (puppet-test-face-at 1))
+    (should-not (puppet-test-face-at 4))
+    (should-not (puppet-test-face-at 6))
+    (should-not (puppet-test-face-at 10))
+    (should-not (puppet-test-face-at 11))))
+
+(ert-deftest puppet-font-lock-keywords/negation ()
+  (should (eq (puppet-test-face-at 1 "!$foo") 'font-lock-negation-char-face)))
+
+(ert-deftest puppet-font-lock-keywords/builtin-metaparameter ()
+  (puppet-test-with-temp-buffer "alias => foo"
+    (should (eq (puppet-test-face-at 1) 'font-lock-builtin-face))
+    (should-not (puppet-test-face-at 7))))
+
+(ert-deftest puppet-font-lock-keywords/builtin-function ()
+  (puppet-test-with-temp-buffer "template('foo/bar')"
+    (should (eq (puppet-test-face-at 1) 'font-lock-builtin-face))
+    (should-not (puppet-test-face-at 9))
+    (should (eq (puppet-test-face-at 10) 'font-lock-string-face))))
+
+(ert-deftest puppet-font-lock-keywords/builtin-function-in-parameter-name ()
+  (puppet-test-with-temp-buffer "require => foo"
+    (should (eq (puppet-test-face-at 1) 'font-lock-builtin-face))
+    (should-not (puppet-test-face-at 10))))
+
+(ert-deftest puppet-font-lock-keywords/variable-expansion-in-sq-string ()
+  (puppet-test-with-temp-buffer "'${::foo::bar} yeah'"
+    (should (eq (puppet-test-face-at 1) 'font-lock-string-face))
+    (should (eq (puppet-test-face-at 2) 'font-lock-warning-face))
+    (should (eq (puppet-test-face-at 3) 'font-lock-warning-face))
+    (should (eq (puppet-test-face-at 4) 'font-lock-warning-face))
+    (should (eq (puppet-test-face-at 6) 'font-lock-warning-face))
+    (should (eq (puppet-test-face-at 14) 'font-lock-warning-face))
+    (should (eq (puppet-test-face-at 16) 'font-lock-string-face))))
+
+(ert-deftest puppet-font-lock-keywords/variable-expansion-in-dq-string ()
+  (puppet-test-with-temp-buffer "\"${::foo::bar} yeah\""
+    (should (eq (puppet-test-face-at 1) 'font-lock-string-face))
+    (should (eq (puppet-test-face-at 2) 'font-lock-variable-name-face))
+    (should (eq (puppet-test-face-at 3) 'font-lock-variable-name-face))
+    (should (eq (puppet-test-face-at 4) 'font-lock-variable-name-face))
+    (should (eq (puppet-test-face-at 6) 'font-lock-variable-name-face))
+    (should (eq (puppet-test-face-at 14) 'font-lock-variable-name-face))
+    (should (eq (puppet-test-face-at 16) 'font-lock-string-face))))
+
+(ert-deftest puppet-font-lock-keywords/variable-expansion-in-comment-disabled ()
+  (let ((puppet-fontify-variables-in-comments nil))
+    (puppet-test-with-temp-buffer "# $foo bar"
+      (should (eq (puppet-test-face-at 1) 'font-lock-comment-delimiter-face))
+      (should (eq (puppet-test-face-at 3) 'font-lock-comment-face))
+      (should (eq (puppet-test-face-at 6) 'font-lock-comment-face))
+      (should (eq (puppet-test-face-at 8) 'font-lock-comment-face)))))
+
+(ert-deftest puppet-font-lock-keywords/variable-expansion-in-comment-enabled ()
+  (let ((puppet-fontify-variables-in-comments t))
+    (puppet-test-with-temp-buffer "# $foo bar"
+      (should (eq (puppet-test-face-at 1) 'font-lock-comment-delimiter-face))
+      (should (eq (puppet-test-face-at 3) 'font-lock-variable-name-face))
+      (should (eq (puppet-test-face-at 6) 'font-lock-variable-name-face))
+      (should (eq (puppet-test-face-at 8) 'font-lock-comment-face)))))
+
+(ert-deftest puppet-font-lock-keywords/escape-in-dq-string ()
+  (puppet-test-with-temp-buffer "\"foo\\n\""
+    (should (eq (puppet-test-face-at 1) 'font-lock-string-face))
+    (should (eq (puppet-test-face-at 5) 'puppet-escape-sequence))
+    (should (eq (puppet-test-face-at 6) 'puppet-escape-sequence))
+    (should (eq (puppet-test-face-at 7) 'font-lock-string-face))))
 
 
 ;;;; Alignment tests
