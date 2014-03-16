6cd7c1c57b7a24f3f50593044d77646006f2ea15
Sebastian Wiesner <lunaryorn@gmail.com>
2014-03-16 19:03:18 +0100

    Add test suite skeleton

---
+++ test/puppet-mode-test.el
@@ -0,0 +1,40 @@
+;;; puppet-mode-test.el --- Puppet Mode: Unit test suite  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2013, 2014  Sebastian Wiesner <lunaryorn@gmail.com>
+;; Copyright (C) 2013, 2014  Bozhidar Batsov <bozhidar@batsov.com>
+
+;; Author: Sebastian Wiesner <lunaryorn@gmail.com>
+;; Maintainer: Bozhidar Batsov <bozhidar@batsov.com>
+;;     Sebastian Wiesner <lunaryorn@gmail.com>
+;; URL: https://github.com/lunaryorn/puppet-mode
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; The unit test suite of Puppet Mode
+
+;;; Code:
+
+
+
+(provide 'puppet-mode-test)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; puppet-mode-test.el ends here
