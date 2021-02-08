voxpupuli/puppet-mode

commit e42b90ab8b04a5dc830ac1ae8f408566e0c2b574

Fix regexp for detecting brace, paren or bracket.
Correction to the regexp modified in commit d8386be; that attempt
basically stopped the regexp from matching at all, which happened to
have the desired effect on my particular test case, but broke other
indentation. This RE actually matches the intent declared in the
comment.

puppet-mode.el
@@ -714,7 +714,7 @@ of the initial include plus puppet-include-indent."
              ;; Brace, paren or bracket (possibly followed by a comma)
              ;; on a line by itself will already be indented to the
              ;; right level, so we can cheat and stop there.
-             ((looking-at "^\\s-*[\)}\]]\\(,\\|\\s-*[-~]>\\)?\\s-*\s?$")
+             ((looking-at "^\\s-*[])}]\\(,\\|\\s-*[-~]>\\)?\\s-*\s?$")
               (setq cur-indent (current-indentation))
               (setq not-indented nil))
 

commit b9441b4eaff4d3f50c7fe597e25e74d4a2f6e018

Handle closing bracket similar to closing paren.
Fixes a failure (introduced by commit e42b90a) of Travis-CI test
"puppet-indent-line/array".

puppet-mode.el
@@ -711,6 +711,13 @@ of the initial include plus puppet-include-indent."
               (setq cur-indent (current-indentation))
               (setq not-indented nil))
 
+             ;; Closing bracket. Use indentation based on start of
+             ;; array.
+             ((looking-at "^[^[\n]*],?\\s-*$")
+              (goto-char (puppet-in-array))
+              (setq cur-indent (current-indentation))
+              (setq not-indented nil))
+
              ;; Brace, paren or bracket (possibly followed by a comma)
              ;; on a line by itself will already be indented to the
              ;; right level, so we can cheat and stop there.